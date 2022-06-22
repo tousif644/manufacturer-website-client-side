10d1026269f4a2712873ddc5ef5e484984007d11
tousif644 <61720566+tousif644@users.noreply.github.com>
2022-06-22 16:49:52 +0600

    Files added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import { React } from 'react';
+function App() {
+
+  return (
+    <div className="App flex justify-center my-24">
+      <h1 className='text-secondary text-5xl font-[Poppins] hover:backdrop-saturate-200'>Helooooooooooo tHrereeeee</h1>
+    </div>
+  )
+}
+
+export default App
