a44dddc5987ce437b1369af027901c48bba4f497
tousif644 <61720566+tousif644@users.noreply.github.com>
2022-07-07 02:37:32 +0600

    on Signout removing the localstorage token

--- src/Pages/Shared/Header.jsx
+++ src/Pages/Shared/Header.jsx
@@ -9,6 +9,7 @@ const Header = () => {
 
     const logOut = () => {
         signOut(auth);
+        localStorage.removeItem('accessToken')
     }
     return (
         <div className=''>
@@ -72,11 +73,11 @@ const Header = () => {
                 </div>
                 <div className='navbar-end'>
                     <Link to="/login" class="btn btn-secondary lg:hidden">Log in</Link>
- <div className='flex navbar-end  '>
-                    <label for="my-drawer-2" class="btn btn-accent drawer-button lg:hidden"><svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg></label>
+                    <div className='flex navbar-end  '>
+                        <label for="my-drawer-2" class="btn btn-accent drawer-button lg:hidden"><svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg></label>
+                    </div>
                 </div>
-                </div>
-               
+
             </div>
         </div>
     );
